5c326d24f11682dc621d7dc5fc9819990708ee3c
jimxshaw <jimxshaw@gmail.com>
2025-11-12 13:48:03 -0600

    Add sample code to connect to PostgreSQL container.

--- app/app.py
+++ app/app.py
@@ -1,10 +1,25 @@
+import psycopg2
 from flask import Flask
 
 app = Flask(__name__)
 
+def test_db_connection():
+  try:
+    conn = psycopg2.connect(
+      host="db", # Must match the service name in docker-compose.yml.
+      database="bytebox",
+      user="postgres",
+      password="postgres"
+    )
+
+    return "Succsessfully connected to PostgreSQL!"
+  except Exception as ex:
+    return f"Failed to connect: {ex}"
+  
+
 @app.route("/")
 def home():
-  return "Hello, this is Flask running in a Docker container!"
+  return test_db_connection()
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5000)
+  app.run(host="0.0.0.0", port=5000, debug=True)
