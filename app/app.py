760a402c54d5ef8582ace3a91fb4e520c289854d
jimxshaw <jimxshaw@gmail.com>
2025-11-19 13:00:44 -0600

    Update app.py with routes.

--- app/app.py
+++ app/app.py
@@ -1,25 +1,34 @@
 import psycopg2
-from flask import Flask
+from flask import Flask, request, redirect, render_template, url_for
 
 app = Flask(__name__)
 
-def test_db_connection():
-  try:
-    conn = psycopg2.connect(
-      host="db", # Must match the service name in docker-compose.yml.
-      database="bytebox",
-      user="postgres",
-      password="postgres"
-    )
+def get_db_connection():
+  conn = psycopg2.connect(
+    host="db", # Must match the service name in docker-compose.yml.
+    database="bytebox",
+    user="postgres",
+    password="postgres"
+  )
+  return conn
 
-    return "Succsessfully connected to PostgreSQL!"
-  except Exception as ex:
-    return f"Failed to connect: {ex}"
+@app.route("/customers")
+def list_customers():
+  conn = get_db_connection()
+  cursor = conn.cursor()
+
+  cursor.execute("SELECT * FROM customer")
   
+  customers = cursor.fetchall()
+
+  cursor.close()
+  conn.close()
+  return render_template("customer_list.html", customers=customers)
+
 
 @app.route("/")
 def home():
-  return test_db_connection()
+  return redirect(url_for("list_customer"))
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=5000, debug=True)
